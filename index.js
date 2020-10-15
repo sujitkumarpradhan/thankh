7da19c0be35ccb4d2226a68e347e177a7783dc27
Pradhan <sujpradh@147dda0bc983.ant.amazon.com>
2020-10-15 23:23:45 +0530

    S3 updated with the new feature request 2.0

--- index.js
+++ index.js
@@ -3,48 +3,34 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+
 import ApolloClient from "apollo-client";
 import { ApolloProvider } from "react-apollo";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { Link, Route, Switch } from "react-router-dom";
-import Presentation from "./Presentation";
-import SFComment from "./SFComment";
-import SendMailValidator from "./sendMailValidator";
-import ClonePresentation from "./clonePresentation";
+import Create from "./Components/pages/create.js";
+import Upload from "./Components/pages/upload.js";
 
-/* import BrowserRouter from 'react-router-dom' */
 import { BrowserRouter } from "react-router-dom";
+import upload from "./Components/pages/upload.js";
 const client = new ApolloClient({
     link: createHttpLink({
         uri:
-            "https://ghoaoclrurc4vf7sksofnxs2cy.appsync-api.us-east-2.amazonaws.com/graphql",
+            "https://a4dbb7grwjdwtadxohnpduwxzm.appsync-api.us-east-1.amazonaws.com/graphql",
         headers: {
-            "x-api-key": "da2-v6no6ouawzdehjjegs57u2bfwe",
-        },
+            "x-api-key": "da2-ifhi74z4dfdjvkedsqq2lwvqnm"
+        }
     }),
-    cache: new InMemoryCache(),
+    cache: new InMemoryCache()
 });
 
 ReactDOM.render(
     <BrowserRouter>
         <ApolloProvider client={client}>
-            <Route exact path="/" component={App} />
-            <Route
-                path={
-                    "/presentation/:itemTab/:itemName/:itemID/:itemEvent/:itemSF"
-                }
-                component={Presentation}
-            />
-            <Route path={"/sfcomment/:itemID"} component={SFComment} />
-            <Route
-                path={"/sendMailValidator/:itemID"}
-                component={SendMailValidator}
-            />
-            <Route
-                path={"/clonepresentation/:itemID"}
-                component={ClonePresentation}
-            />
+            <Route exact path="/" component={Create} />
+            <Route exact path="/home" component={Create} />
+            <Route exact path="/upload" component={Upload} />
         </ApolloProvider>
     </BrowserRouter>,
     document.getElementById("root")
